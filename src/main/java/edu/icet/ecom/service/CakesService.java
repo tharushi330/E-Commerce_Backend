0a95d2e6354ed8fa9f42eff5fda4b5eb43a84690
tharushi330 <tharushiwarangana78@gmail.com>
2025-05-15 07:30:36 +0530

    create service

--- src/main/java/edu/icet/ecom/service/CakesService.java
+++ src/main/java/edu/icet/ecom/service/CakesService.java
@@ -1,4 +1,18 @@
 package edu.icet.ecom.service;
 
+import edu.icet.ecom.dto.Cakes;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface CakesService {
+    Optional<Cakes> add(Cakes cakes);
+
+    void delete(Long id);
+
+    void update(Cakes cakes);
+
+    List<Cakes> searchByName(String name);
+
+    List<Cakes> getAll();
 }
--- src/main/java/edu/icet/ecom/service/impl/CakesServiceImpl.java
+++ src/main/java/edu/icet/ecom/service/impl/CakesServiceImpl.java
@@ -1,10 +1,38 @@
 package edu.icet.ecom.service.impl;
 
+import edu.icet.ecom.dto.Cakes;
 import edu.icet.ecom.service.CakesService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class CakesServiceImpl implements CakesService {
+    @Override
+    public Optional<Cakes> add(Cakes cakes) {
+        return Optional.empty();
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
+
+    @Override
+    public void update(Cakes cakes) {
+
+    }
+
+    @Override
+    public List<Cakes> searchByName(String name) {
+        return List.of();
+    }
+
+    @Override
+    public List<Cakes> getAll() {
+        return List.of();
+    }
 }
